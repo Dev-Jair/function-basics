4da5f0c5b3bdc85844550c4cbe8c9200289fce91
Dev-Jair <86578182+Dev-Jair@users.noreply.github.com>
2021-08-07 22:48:57 +0200

    Add files via upload

---
+++ Opdracht_function_basics/script.js
@@ -0,0 +1,51 @@
+// Function basics: making pizza
+
+const makePizza = function () {
+    console.log("Step 1: Knead the dough");
+    console.log("Step 2: Shape the dough");
+    console.log("Step 3: Top the pizza and Bake");
+}
+
+makePizza();
+
+makePizza();
+
+makePizza();
+
+//Function basics: making sushi
+
+const makeSushi = function () {
+    console.log("Step 1: Prep the rice & Nori. Lay out the nori on the bamboo mat");
+    console.log("Step 2: Spread the rice on the Nori");
+    console.log("Step 3: Add fillings to the bottom quarter of the Nori");
+    console.log("Step 4: Roll the Sushi part way");
+    console.log("Step 5: Roll the sushi the rest of the way");
+    console.log("Step 6: Move to cutting board");
+    console.log("Step 7: Cut the sushi & serve!");
+}
+
+makeSushi();
+
+// Pretending to work at a restaurant that gets an order for three meals of sushi and two pizzas:
+
+console.log("Order #1326: First sushi meal:");
+
+makeSushi();
+
+console.log("Order #1326: Second sushi meal:");
+
+makeSushi();
+
+console.log("Order #1326: Third sushi meal:");
+
+makeSushi();
+
+console.log("Order #1326: First pizza meal:");
+
+makePizza();
+
+console.log("Order #1326: Second pizza meal:");
+
+makePizza();
+
+
